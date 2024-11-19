f6557c131080d048308d806ad7e48931d04a5409
Bart Frackiewicz <decomplosed@gmail.com>
2024-11-19 18:51:51 +0100

    Add functionality to remove todo item

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import List
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def home(request):
-	all_items = List.objects.all
-	return render(request, 'home.html', {'all_items': all_items})
+		if request.method == "POST":
+				form = ListForm(request.POST or None)
+
+				if form.is_valid():
+						form.save()
+						all_items = List.objects.all
+						messages.success(request, ('Item has been added to the list!'))
+						return render(request, 'home.html', {'all_items': all_items})		
+		else:
+				all_items = List.objects.all
+				return render(request, 'home.html', {'all_items': all_items})
 
 def about(request):
-	return render(request, 'about.html', {})
+		return render(request, 'about.html', {})
+
+def delete(request, list_id):
+		item = List.objects.get(pk=list_id)
+		item.delete()
+		messages.success(request, ('Item has been deleted!'))
+		return redirect('home')
